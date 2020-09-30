42d3cfe7998f289f8e1c3e387c9433f86ed4fe9c
Aris Vangelis <Aris.VANGELIS@intrasoft-intl.com>
2020-09-30 09:13:03 +0300

    develop add router

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
 import React from "react";
 import "./App.css";
-import Examinations from "./pages/examinations/examinations.compnent";
+import Examinations from "./pages/examinations/examinations.component";
 import { Switch, Route } from "react-router";
 import { BrowserRouter as Router } from "react-router-dom";
+import AppRouter from "./AppRouter";
 
 import Signin from "./components/sign-in/sign-in.component";
 
 const App = () => {
+  const log = false;
   return (
-    <Router initialEntries={["/examinations"]} initialIndex={0}>
+    <Router initialEntries={["/create"]}>
       <Switch>
-        <Route path="/resetPasswordForm">
-          {
-            <>
-              <Signin />
-            </>
-          }
-        </Route>
-
-        {/* <Route path="/">{loggedIn ? <AppRouter /> : <LoginPageUi />}</Route> */}
+        <Route path="/">{log ? <AppRouter /> : <Signin />}</Route>
       </Switch>
     </Router>
   );
--- src/AppRouter.js
+++ src/AppRouter.js
@@ -1,27 +1,12 @@
 import React from "react";
 import { Switch, Route } from "react-router";
-import Examinations from "./pages/examinations";
-
-const useStyles = makeStyles({
-  main: {
-    paddingTop: appBarHeight,
-  },
-});
+import Examinations from "./pages/examinations/examinations.component";
 
 const AppRouter = () => {
-  const styles = useStyles();
   return (
-    <>
-      <Header />
-      <main className={styles.main}>
-        <Switch>
-          <Route path="/examinations" render={() => <Examinations />} />
-          {/* <Route path="/my-account" render={() => <MyAccount />} />
-          <Route path="/error" render={() => <Error />} />
-          <Route path="/login" render={() => <Login />} /> */}
-        </Switch>
-      </main>
-    </>
+    <Switch>
+      <Route path="/examinations" render={() => <Examinations />} />
+    </Switch>
   );
 };
 
